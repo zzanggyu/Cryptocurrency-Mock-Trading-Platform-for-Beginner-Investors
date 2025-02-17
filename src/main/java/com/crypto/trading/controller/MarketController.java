9c30331e046c1f391d08b75161f089d9eca49663
JeongWogh <wogh06@gmail.com>
2025-02-17 16:09:44 +0900

    메인 화면 뉴스 출력

--- src/main/java/com/crypto/trading/controller/MarketController.java
+++ src/main/java/com/crypto/trading/controller/MarketController.java
@@ -1,19 +1,29 @@
 package com.crypto.trading.controller;
 
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import com.crypto.trading.entity.News;
+import com.crypto.trading.service.NewsService;
+
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class MarketController {
    RestTemplate restTemplate = new RestTemplate();  // HTTP 요청을 위한 RestTemplate 객체
+   private final NewsService newsService;
    
    @GetMapping("/market/ticker")  // 코인 시세 조회
    public ResponseEntity<?> getMarketTicker() {
@@ -27,6 +37,18 @@ public class MarketController {
        } catch (Exception e) {
            System.out.println("Ticker Error: " + e.getMessage());  // 에러 로그
            return ResponseEntity.badRequest().body("시세 조회 실패 : " + e.getMessage());
+       }
+   }
+   
+   @GetMapping("/market/news")
+   public ResponseEntity<?> getLatestNews() {
+       try {
+           List<News> newsList = newsService.getLatestNews();
+           System.out.println("뉴스 조회 결과: " + newsList);
+           return ResponseEntity.ok(newsList);
+       } catch (Exception e) {
+           System.out.println("News Error: " + e.getMessage());
+           return ResponseEntity.badRequest().body("뉴스 조회 실패: " + e.getMessage());
        }
    }
 }
